205391c64a4d8748f69d57d56dd50b307e0c5cb4
Tim Rosine <trosine@comscore.com>
2016-04-26 11:31:15 -0400

    Hack hiera merge to work with puppet 3.x

--- manifests/master/config/hiera/merge.pp
+++ manifests/master/config/hiera/merge.pp
@@ -14,11 +14,37 @@ class ospuppet::master::config::hiera::merge {
     provider        => $puppet_gem_provider,
   }
 
-  package { "puppetserver.${hiera_merge_package_name}":
-    ensure          => $hiera_merge_package_version,
-    name            => $hiera_merge_package_name,
-    install_options => $gem_provider_install_options,
-    provider        => $puppetserver_gem_provider,
-  }
+  if (defined(Class['ospuppet::server'])) {
+    # it is only necessary to install the puppetserver gem with puppetserver
 
+    if (versioncmp($::serverversion,'4.0.0') >= 0) {
+
+      # the puppetserver_gem provider only works with puppet >= 4.0
+      package { "puppetserver.${hiera_merge_package_name}":
+        ensure          => $hiera_merge_package_version,
+        name            => $hiera_merge_package_name,
+        install_options => $gem_provider_install_options,
+        provider        => $puppetserver_gem_provider,
+      }
+
+    }
+    elsif ($hiera_merge_package_version != 'absent') {
+
+      if ($hiera_merge_package_version == 'latest') {
+        warning("ospuppet::master: For Puppet < 4.0, ensuring hiera-merge to 'latest' is equivalent to 'installed'")
+      }
+      case $hiera_merge_package_version {
+        /^(present|installed)$/: { $install_version = "" }
+        /^latest$/:              { $install_version = "" }
+        default:                 { $install_version = "--version ${hiera_merge_package_version}" }
+      }
+
+      exec { "puppetserver.${hiera_merge_package_name}":
+        command => "puppetserver gem install ${hiera_merge_package_name} ${gem_provider_install_options} ${install_version}",
+        onlyif  => "puppetserver gem specification --local ${hiera_merge_package_name} name 2>&1 | grep ^ERROR:",
+        require => Class['ospuppet::server::config']
+      }
+
+    }
+  }
 }
